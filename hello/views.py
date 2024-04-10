dec54f835803e0f167fbaf92f9e5037a7b53c991
EastonA01 <eastonredwing@gmail.com>
2024-04-10 00:42:58 -0400

    Initial Commit

---
+++ hello/views.py
@@ -0,0 +1,50 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+    # TODO: Add a check using the profanity API to filter out the message's harsh words-if any.
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+    
+# def profanity_check(message):
+#     if client.contains_profanity(message) == True:
+#         raise Exception("Sorry, no swearing")
